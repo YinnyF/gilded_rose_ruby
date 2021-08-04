7ceb6cb544b09260118dfc563dc89df5c1bef512
Cynthia Fu <yinnycfu@gmail.com>
2021-08-04 23:26:46 +0100

    test: feature - backstage passes quality rules

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -7,9 +7,10 @@ describe 'gilded_rose' do
     Item.new(name = "Aged Brie", sell_in = 2, quality = 0),
     Item.new(name = "Sulfuras, Hand of Ragnaros", sell_in = 0, quality = 80),
     Item.new(name = "Sulfuras, Hand of Ragnaros", sell_in = -1, quality = 80),
-    # Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 15, quality = 20),
-    # Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 10, quality = 49),
-    # Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 5, quality = 49),
+    Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 15, quality = 20),
+    Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 10, quality = 20),
+    Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 5, quality = 20),
+    Item.new(name = "Backstage passes to a TAFKAL80ETC concert", sell_in = 0, quality = 20),
     # # This Conjured item does not work properly yet
     # Item.new(name = "Conjured Mana Cake", sell_in = 3, quality = 6), # <-- :O
     ]
@@ -57,4 +58,20 @@ describe 'gilded_rose' do
     expect { gilded_rose.update_quality() }.to change { items[3].quality }.by(0)
     expect { gilded_rose.update_quality() }.to change { items[4].quality }.by(0)
   end
+
+  it "Backstage passes increases in Quality when sell_in date is greater than 10" do
+    expect { gilded_rose.update_quality() }.to change { items[5].quality }.by(1)
+  end
+
+  it "Backstage passes increases by 2 Quality when sell_in date is <= 10 but > 5" do
+    expect { gilded_rose.update_quality() }.to change { items[6].quality }.by(2)
+  end
+
+  it "Backstage passes increases by 3 Quality when sell_in date is <= 5 but > 0" do
+    expect { gilded_rose.update_quality() }.to change { items[7].quality }.by(3)
+  end 
+
+  it "Backstage passes has 0 Quality when sell_in date is < 0" do
+    expect { gilded_rose.update_quality() }.to change { items[8].quality }.by(-20)
+  end 
 end
