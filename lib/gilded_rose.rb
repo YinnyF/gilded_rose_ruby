cbb70184c0c0bd1d5c6fd982f55531e29d9a7238
Cynthia Fu <yinnycfu@gmail.com>
2021-08-05 22:35:24 +0100

    refactor: add private method for get item quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,23 +9,23 @@ class GildedRose
   def update_quality
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
+        if get_quality(item) > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            item.quality = get_quality(item) - 1
           end
         end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
+        if get_quality(item) < 50
+          item.quality = get_quality(item) + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
+              if get_quality(item) < 50
+                item.quality = get_quality(item) + 1
               end
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
+              if get_quality(item) < 50
+                item.quality = get_quality(item) + 1
               end
             end
           end
@@ -37,20 +37,26 @@ class GildedRose
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
+            if get_quality(item) > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                item.quality = get_quality(item) - 1
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = get_quality(item) - get_quality(item)
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            item.quality = get_quality(item) + 1
           end
         end
       end
     end
   end
+end
+
+private
+
+def get_quality(item)
+  item.quality
 end
