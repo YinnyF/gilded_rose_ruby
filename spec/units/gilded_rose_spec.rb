24f518b294783826c021cd02a4939759f2b60070
Cynthia Fu <yinnycfu@gmail.com>
2021-08-05 19:02:17 +0100

    test: unit - regular item sell in -1, quality -1

--- spec/units/gilded_rose_spec.rb
+++ spec/units/gilded_rose_spec.rb
@@ -1,17 +1,6 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  let(:item_0) { double(Item, name: "+5 Dexterity Vest", sell_in: 10, quality: 20) }
-  let(:item_1) { double(Item, name: "+5 Dexterity Vest", sell_in: 0, quality: 20) }
-  let(:item_2) { double(Item, name: "Aged Brie", sell_in: 2, quality: 0) }
-  let(:item_3) { double(Item, name: "Sulfuras, Hand of Ragnaros", sell_in: 0, quality: 80) }
-  let(:item_4) { double(Item, name: "Sulfuras, Hand of Ragnaros", sell_in: -1, quality: 80) }
-  let(:item_5) { double(Item, name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 15, quality: 20) }
-  let(:item_6) { double(Item, name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 10, quality: 20) }
-  let(:item_7) { double(Item, name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 20) }
-  let(:item_8) { double(Item, name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 0, quality: 20) }
-  let(:item_9) { double(Item, name: "Conjured Mana Cake", sell_in: 3, quality: 6) }
-  subject { described_class.new(items) }
 
   describe "#update_quality" do
     it "does not change the name" do
@@ -22,6 +11,22 @@ describe GildedRose do
       gilded_rose.update_quality()
       
       expect(items[0].name).to eq "foo"
+    end
+
+    context "a regular item (nothing special)" do
+      let(:regular_item) { double(Item, name: "+5 Dexterity Vest", sell_in: 10, quality: 20, 'sell_in=': 'sell in changed', 'quality=': 'quality changed') }
+      let(:items) { [regular_item] }
+      subject { described_class.new(items) }
+
+      it "amends the sell_in value of an item by -1" do
+        expect(regular_item).to receive(:sell_in=).with(regular_item.sell_in - 1)
+        subject.update_quality()
+      end
+
+      it "amends the quality of an item by -1" do
+        expect(regular_item).to receive(:quality=).with(regular_item.quality - 1)
+        subject.update_quality()
+      end
     end
 
   end
