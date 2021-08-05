fb0ad5eca01c0fc59458b5c5e838182811243475
Cynthia Fu <yinnycfu@gmail.com>
2021-08-05 22:54:28 +0100

    refactor: add private method for increase item quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -16,16 +16,16 @@ class GildedRose
         end
       else
         if get_quality(item) < 50
-          item.quality = get_quality(item) + 1
+          increase_quality(item)
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if get_quality(item) < 50
-                item.quality = get_quality(item) + 1
+                increase_quality(item)
               end
             end
             if item.sell_in < 6
               if get_quality(item) < 50
-                item.quality = get_quality(item) + 1
+                increase_quality(item)
               end
             end
           end
@@ -47,7 +47,7 @@ class GildedRose
           end
         else
           if item.quality < 50
-            item.quality = get_quality(item) + 1
+            increase_quality(item)
           end
         end
       end
@@ -59,4 +59,8 @@ private
 
 def get_quality(item)
   item.quality
+end
+
+def increase_quality(item)
+  item.quality = get_quality(item) + 1
 end
