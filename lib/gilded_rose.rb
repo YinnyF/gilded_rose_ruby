f6770e065c9fe1c7b0eff8a560fb32ce9f33dde0
Cynthia Fu <yinnycfu@gmail.com>
2021-08-06 17:03:57 +0100

    refactor: change quality of specific item - private methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -13,25 +13,14 @@ class GildedRose
   def update_quality
     @items.each do |item|
       next if sulfuras?(item)
-
       decrease_sell_in(item)
 
       if brie?(item)
-        increase_quality(item)
-        increase_quality(item) if passed_sell_by_date?(item)
+        change_quality_of_brie(item)
       elsif backstage_pass?(item)
-        if passed_sell_by_date?(item)
-          make_quality_0(item)
-        elsif get_sell_in(item) <= DAYS_TO_CONCERT_CLOSER # days to concert <= 5
-          3.times { increase_quality(item) }
-        elsif get_sell_in(item) <= DAYS_TO_CONCERT_CLOSE # days to concert <= 10
-          2.times { increase_quality(item) }
-        else # days to concert > 10
-          increase_quality(item)
-        end
+        change_quality_of_backstage_pass(item)
       else # regular item
-        decrease_quality(item) if passed_sell_by_date?(item)
-        decrease_quality(item)
+        change_quality_of_regular(item)
       end
 
     end
@@ -89,5 +78,35 @@ class GildedRose
 
   def backstage_pass?(item)
     item.name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+  def change_quality_of_brie(item)
+    increase_quality(item)
+    increase_quality(item) if passed_sell_by_date?(item)
+  end
+
+  def days_to_concert_less_than_5?(item)
+    get_sell_in(item) <= DAYS_TO_CONCERT_CLOSER
+  end
+
+  def days_to_concert_less_than_10?(item)
+    get_sell_in(item) <= DAYS_TO_CONCERT_CLOSE
+  end
+
+  def change_quality_of_backstage_pass(item)
+    if passed_sell_by_date?(item)
+      make_quality_0(item)
+    elsif days_to_concert_less_than_5?(item)
+      3.times { increase_quality(item) }
+    elsif days_to_concert_less_than_10?(item)
+      2.times { increase_quality(item) }
+    else # days to concert > 10
+      increase_quality(item)
+    end
+  end
+
+  def change_quality_of_regular(item)
+    decrease_quality(item) if passed_sell_by_date?(item)
+    decrease_quality(item)
   end
 end
